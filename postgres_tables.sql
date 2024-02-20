0ab5ea536fb62c255e8bd4fcc79351972955dfee
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-02-20 16:42:18 +0530

    Added file for SQL Queries

---
+++ postgres_tables.sql
@@ -0,0 +1,69 @@
+CREATE TABLE sump1_data (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    temperature FLOAT,
+    voltage FLOAT,
+    uncompensated_tds FLOAT,
+    compensated_tds FLOAT,
+	turbudity FLOAT,
+	ph Float
+);
+
+CREATE TABLE oht1_data (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    temperature FLOAT,
+    voltage FLOAT,
+    uncompensated_tds FLOAT,
+    compensated_tds FLOAT,
+	turbudity FLOAT,
+	ph Float
+);
+
+CREATE TABLE roplant1_data (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    temperature FLOAT,
+    voltage FLOAT,
+    uncompensated_tds FLOAT,
+    compensated_tds FLOAT
+);
+
+CREATE TABLE roplant1_tds (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    tds FLOAT
+);
+
+CREATE TABLE ro1_data (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    temperature FLOAT,
+    voltage FLOAT,
+    uncompensated_tds FLOAT,
+    compensated_tds FLOAT
+);
+
+CREATE TABLE ro2_data (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    temperature FLOAT,
+    voltage FLOAT,
+    uncompensated_tds FLOAT,
+    compensated_tds FLOAT
+);
+
+CREATE TABLE ro3_data (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    temperature FLOAT,
+    voltage FLOAT,
+    uncompensated_tds FLOAT,
+    compensated_tds FLOAT
+);
+
+CREATE TABLE motor1_status (
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	creationtime TIMESTAMP WITH TIME ZONE,
+    status FLOAT
+);
