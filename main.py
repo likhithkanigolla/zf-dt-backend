7784bd9dfe2b399cd79a3fa8b53ffb153f9d4bf8
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-01 23:55:21 +0530

    changed backend port from 8080 to 1629

--- main.py
+++ main.py
@@ -88,4 +88,4 @@ async def water_level_sub(table_name: str, data: dict):
     
 if __name__=='__main__':
     import uvicorn
-    uvicorn.run(app,host="0.0.0.0",port=8080)
+    uvicorn.run(app,host="0.0.0.0",port=1629)
