7e77ea08c16f61cfcfa33c1665ac00e9b41a4ea2
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-15 13:39:29 +0530

    Added db parameter

--- modules/device_routes.py
+++ modules/device_routes.py
@@ -6,13 +6,13 @@ router = APIRouter()
 
 @router.post("/waterqualitysub/{table_name}")
 async def water_quality_sub(table_name: str, data: dict):
-    return data_processing.process_water_quality_sub(table_name, data)
+    return data_processing.process_water_quality_sub(db,table_name, data)
 
 
 @router.post("/waterlevelsub/{table_name}")
 async def water_level_sub(table_name: str, data: dict):
-    return data_processing.process_water_level_sub(table_name, data)
+    return data_processing.process_water_level_sub(db,table_name, data)
 
 @router.post("/motorsub/{table_name}")
 async def motor_sub(table_name: str, data: dict):
-    return data_processing.process_motor_sub(table_name, data)
+    return data_processing.process_motor_sub(db,table_name, data)
