8441c43e0de03a27ec42add8b74dab1782bc21df
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-15 13:35:32 +0530

    passing db to the device routes

--- modules/device_routes.py
+++ modules/device_routes.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 from modules import data_processing
+from modules.database import db
 
 router = APIRouter()
 
 @router.post("/waterqualitysub/{table_name}")
 async def water_quality_sub(table_name: str, data: dict):
     return data_processing.process_water_quality_sub(table_name, data)
+
 
 @router.post("/waterlevelsub/{table_name}")
 async def water_level_sub(table_name: str, data: dict):
