8491ed22b445644667d25a496144ab1deff4d5b5
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-15 12:33:50 +0530

    Updated Database File to autocreate tables

--- modules/database.py
+++ modules/database.py
@@ -4,7 +4,7 @@ import os
 class Database:
     def __init__(self):
         # Define your PostgreSQL connection parameters
-        self.DB_NAME = 'zf_digitaltwin'
+        self.DB_NAME = 'testdb'
         self.DB_USER = 'likhith'
         self.DB_PASSWORD = 'postgres'
         self.DB_HOST = 'localhost'
@@ -25,10 +25,23 @@ class Database:
 
     def create_tables(self):
         # Read SQL file
-        sql_file = os.path.join(os.path.dirname(__file__), 'sql', 'postgres_tables.sql')
+        sql_file = os.path.join(os.path.dirname(__file__), '../sql', 'postgres_tables.sql')
         with open(sql_file, 'r') as file:
             sql_statements = file.read()
 
-        # Execute SQL statements
-        self.cur.execute(sql_statements)
-        self.conn.commit()
+        # Split SQL statements into individual statements
+        statements = sql_statements.split(';')
+
+        # Execute each SQL statement
+        try:
+            for statement in statements:
+                # Remove leading/trailing whitespace and skip empty statements
+                statement = statement.strip()
+                if statement:
+                    # Attempt to execute the SQL statement
+                    self.cur.execute(statement)
+            self.conn.commit()
+        except Exception as e:
+            # Handle any exceptions that occur during table creation
+            print(f"Error creating tables: {str(e)}")
+            self.conn.rollback()
