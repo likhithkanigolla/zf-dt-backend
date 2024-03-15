c5a5e4d988fa39e624c3a42167cdfe42c57db2f8
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-15 13:42:40 +0530

    DB fix #3

--- modules/device_routes.py
+++ modules/device_routes.py
@@ -5,14 +5,13 @@ from modules.database import db
 router = APIRouter()
 
 @router.post("/waterqualitysub/{table_name}")
-async def water_quality_sub(db,table_name: str, data: dict):
-    return data_processing.process_water_quality_sub(db,table_name, data)
-
+async def water_quality_sub(table_name: str, data: dict):
+    return data_processing.process_water_quality_sub(db, table_name, data)
 
 @router.post("/waterlevelsub/{table_name}")
-async def water_level_sub(db,table_name: str, data: dict):
-    return data_processing.process_water_level_sub(db,table_name, data)
+async def water_level_sub(table_name: str, data: dict):
+    return data_processing.process_water_level_sub(db, table_name, data)
 
 @router.post("/motorsub/{table_name}")
-async def motor_sub(db,table_name: str, data: dict):
-    return data_processing.process_motor_sub(db,table_name, data)
+async def motor_sub(table_name: str, data: dict):
+    return data_processing.process_motor_sub(db, table_name, data)
