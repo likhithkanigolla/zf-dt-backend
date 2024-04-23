837290542e357a80648ea19d06dda72efa762c11
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-04-23 20:40:00 +0530

    Updated OneM2M function for actuation

--- modules/post_data.py
+++ modules/post_data.py
@@ -1,22 +1,22 @@
 import json
 import requests
+import time
 
-BACKEND_URL="http://onem2m.iiit.ac.in:443/~/in-cse/AE-DM/"
+BACKEND_URL="http://onem2m.iiit.ac.in:443/~/in-cse/in-name/AE-DM/"
 
-def post_to_onem2m(NodeID,data):
-    url = BACKEND_URL+NodeID+"/Control"
-    data = str(data)
-    data_json = json.dumps(data)
+def post_to_onem2m_act(node_type, node_name, status):
+    epoch_time = int(time.time())
+    data_list = [epoch_time,node_type,status]  # Initialize the data list with some default values
+    url = BACKEND_URL + node_name + "/Control"
+    data_list=str(data_list)
     payload = json.dumps({
         "m2m:cin": {
-            "con": data
+            "con": data_list
         }
     })
     headers = {
-    'X-M2M-Origin': 'admin:admin',
-    'Content-Type': 'application/json;ty=4'
+        'X-M2M-Origin': 'DeviceMon@20:9G&6OnuL1iZ',
+        'Content-Type': 'application/json;ty=4'
     }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text)
+    response = requests.post(url, headers=headers, data=payload)
+    return response.text
