27940f88d8ccc2679f06506dc46f685fd2655360
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-05-14 23:19:51 +0530

    Moved Models

--- ml-models/test_model.py
+++ ml-models/test_model.py
@@ -1,29 +1,11 @@
 import joblib
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.preprocessing import StandardScaler
-import numpy as np
 
-# Load the trained model from the file
-loaded_model = joblib.load("polynomial_regression_model.pkl")
+# Load the model from the file
+model = joblib.load('./random_forest_model.pkl')
 
-# Define the polynomial features transformer
-degree = 3
-poly_features = PolynomialFeatures(degree=degree)
+# temp, quantity, sand 
+# Make predictions on new data
+new_data = [[19.3125, 6000, 1809]]
+predictions = model.predict(new_data)
 
-# Load the scaler used for scaling the target variable during training
-target_scaler = joblib.load("target_scaler.pkl")
-
-# Take input from the user for prediction
-input_features = np.array([30, 1, 100]).reshape(1, -1)
-
-# Transform the input features using polynomial features
-input_features_poly = poly_features.transform(input_features)
-
-# Make predictions using the loaded model
-predicted_value_scaled = loaded_model.predict(input_features_poly)
-
-# Unscaled the predicted value
-predicted_value = target_scaler.inverse_transform(predicted_value_scaled.reshape(-1, 1))
-
-# Print the predicted value
-print("Predicted value:", predicted_value)
+print(predictions)
