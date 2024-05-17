9a7854a42af32bf0017cada20943355e28869128
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-05-17 11:31:23 +0530

    Added some comments and changed to take the variables

--- modules/config/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env
-DATABASE_URL = os.getenv("DEMO_TEXT")
--- modules/database.py
+++ modules/database.py
@@ -1,14 +1,17 @@
 import psycopg2
 import os
+from dotenv import load_dotenv
 
 class Database:
     def __init__(self):
+        dotenv_path = os.path.join(os.path.dirname(__file__), './config', '.env')
+        load_dotenv(dotenv_path)
         # Define your PostgreSQL connection parameters
-        self.DB_NAME = 'zf_digitaltwin'
-        self.DB_USER = 'likhith'
-        self.DB_PASSWORD = 'postgres'
-        self.DB_HOST = 'localhost'
-        self.DB_PORT = '5432'
+        self.DB_NAME = os.getenv('DB_NAME')
+        self.DB_USER = os.getenv('DB_USER')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD')
+        self.DB_HOST = os.getenv('DB_HOST')
+        self.DB_PORT = os.getenv('DB_PORT')
 
         # Connect to PostgreSQL
         self.conn = psycopg2.connect(
