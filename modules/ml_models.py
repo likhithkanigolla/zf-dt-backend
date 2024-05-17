b8b5c791b40ec6b2b164571da809a3246a435e20
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-05-17 19:54:29 +0530

    Load ML Models from a configuration file #2

--- modules/ml_models.py
+++ modules/ml_models.py
@@ -1,18 +1,29 @@
 import joblib
+import yaml
 import xgboost
 import numpy as np
 import os
 
-def get_soil_module():
-    """
-    Load the soil contamination model and scaler from the disk
-    :return: Tuple of the loaded model and the scaler
-    """
-    current_dir = os.path.dirname(__file__)
-    soil_model_path = os.path.join(current_dir, 'data_models', 'soil_XGBoost.pkl')
-    soil_scaler_path = os.path.join(current_dir, 'data_models', 'soil_scaler.pkl')
-    soil_model = joblib.load(soil_model_path)
-    soil_scaler = joblib.load(soil_scaler_path)
+def load_soil_module():
+    root_dir = os.path.dirname(os.path.abspath(__file__))  # Get the current directory of the script
+    root_dir = os.path.dirname(root_dir)  # Get the parent directory
+    # print("Root directory: ", root_dir)
+    config_path = os.path.join(root_dir, 'configuration.yaml')
+    # print("Config path: ", config_path)
+    # Read the configuration file and extract the file paths
+    with open(config_path, 'r') as config_file:
+        config_data = yaml.load(config_file, Loader=yaml.SafeLoader)  # Assuming configuration.yaml is in YAML format
+        models = config_data['models']
+        soil_model_path = os.path.join(root_dir, models[0]['path'])
+        soil_scaler_path = os.path.join(root_dir, models[1]['path'])
+    # print("Soil model path: ", soil_model_path)
+    # print("Soil scaler path: ", soil_scaler_path)
+    
+    try:
+        soil_model = joblib.load(soil_model_path)
+        soil_scaler = joblib.load(soil_scaler_path)
+        print("Soil Models loaded successfully.")
+    except Exception as e:
+        print("Error loading soil models:", str(e))
+
     return soil_model, soil_scaler
-
-
