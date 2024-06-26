16ade01f08ffc35b1a0f1fc125affc4351ff946b
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-06-26 19:08:09 +0530

    Query FIx

--- sql/postgres_tables.sql
+++ sql/postgres_tables.sql
@@ -1,6 +1,6 @@
 -- Creation of Tables 
 -- Authenticaltion Users Table 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     hashed_password VARCHAR(255) NOT NULL
