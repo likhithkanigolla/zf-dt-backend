d63e92dd112d31a649f1b345f9590d9ce4f1ccce
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-06-27 16:59:45 +0530

    Updated Length

--- sql/postgres_tables.sql
+++ sql/postgres_tables.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    hashed_password VARCHAR(255) NOT NULL
+    hashed_password VARCHAR(2550) NOT NULL
 );
 
 
